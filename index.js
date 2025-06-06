8b5dd3c497ba1aa75bcd1aa4a3a29ca67c5c8078
root <root@ip-172-31-40-194.us-east-2.compute.internal>
2025-06-06 08:20:30 +0000

    added changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
-  res.end('Hello Bhai!');
+  res.end('Helloo Bhai!');
 });
 
 server.listen(PORT, () => {
